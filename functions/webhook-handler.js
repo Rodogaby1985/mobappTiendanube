f84fa713a7cdb7f97644764888e4b05f53c1f7fa
Rodogaby1985 <rodogaby85@gmail.com>
2025-07-07 22:21:24 -0300

    Update webhook-handler.js

--- functions/webhook-handler.js
+++ functions/webhook-handler.js
@@ -5,30 +5,24 @@ exports.handler = async function(event, context) {
   console.log("Webhook recibido:", topic);
   console.log("Datos:", data);
 
-  // Manejo por tipo de evento
   switch(topic) {
     case 'app/suspended':
-      console.log("La aplicación fue suspendida:", data.store_id);
+      console.log("La app fue suspendida:", data.store_id);
       break;
-
     case 'app/resumed':
-      console.log("La aplicación fue reanudada:", data.store_id);
+      console.log("La app fue reactivada:", data.store_id);
       break;
-
     case 'store/redact':
-      console.log("Se solicitó redactar datos de tienda:", data.store_id);
+      console.log("Redactando datos de tienda:", data.store_id);
       break;
-
     case 'customers/redact':
-      console.log("Se solicitó redactar clientes:", data.customer_ids);
+      console.log("Redactando clientes:", data.customer_ids);
       break;
-
     case 'customer/data_request':
-      console.log("Cliente solicitó sus datos:", data.customer_id);
+      console.log("Solicitud de datos de cliente:", data.customer_id);
       break;
-
     default:
-      console.log("Evento no reconocido:", topic);
+      console.log("Evento no reconocido");
   }
 
   return {
